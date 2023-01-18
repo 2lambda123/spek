49f32a00bd7caf4eced44d3ecac41940dbd16b39
Christian Williams <antixian666@gmail.com>
2023-01-18 10:47:50 -0800

    Bump gradle-intellij-plugin from 1.4.0 to 1.12.0, so it will build with JDK 8 maybe?

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,6 +13,6 @@ dependencies {
     api(kotlin("gradle-plugin", version = "1.5.31"))
     api("org.jetbrains.dokka:dokka-gradle-plugin:0.9.17")
     api("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
-    api("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.4.0")
+    api("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.12.0")
     api("com.github.jengelman.gradle.plugins:shadow:4.0.2")
 }
